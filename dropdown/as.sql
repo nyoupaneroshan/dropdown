66fc3136e35206b213187b427df488cce2feb607
nyoupaneroshan <35588093+nyoupaneroshan@users.noreply.github.com>
2020-04-06 09:59:54 +0545

    Add files via upload

---
+++ dropdown/as.sql
@@ -0,0 +1,21 @@
+INSERT INTO `city` (`id`, `name`, `codes`, `state_id`) VALUES (NULL, 'Doti D.P.O', '10900', '75');
+INSERT INTO `city` (`id`, `name`, `codes`, `state_id`) VALUES (NULL, 'Silgadi', '10901', '75');
+INSERT INTO `city` (`id`, `name`, `codes`, `state_id`) VALUES (NULL, 'Sanagaun', '10902', '75');
+INSERT INTO `city` (`id`, `name`, `codes`, `state_id`) VALUES (NULL, 'Dound', '10903', '75');
+INSERT INTO `city` (`id`, `name`, `codes`, `state_id`) VALUES (NULL, 'Mauwa Nagardaha', '10904', '75');
+INSERT INTO `city` (`id`, `name`, `codes`, `state_id`) VALUES (NULL, 'Banedungrasen', '10905', '75');
+INSERT INTO `city` (`id`, `name`, `codes`, `state_id`) VALUES (NULL, 'Jorayal', '10906', '75');
+INSERT INTO `city` (`id`, `name`, `codes`, `state_id`) VALUES (NULL, 'Gridhshera', '10907', '75');
+INSERT INTO `city` (`id`, `name`, `codes`, `state_id`) VALUES (NULL, 'Lankedareshwor', '10908', '75');
+INSERT INTO `city` (`id`, `name`, `codes`, `state_id`) VALUES (NULL, 'Boktan', '10909', '75');
+INSERT INTO `city` (`id`, `name`, `codes`, `state_id`) VALUES (NULL, 'Bael', '10910', '75');
+INSERT INTO `city` (`id`, `name`, `codes`, `state_id`) VALUES (NULL, 'Mudbhara', '10911', '75');
+INSERT INTO `city` (`id`, `name`, `codes`, `state_id`) VALUES (NULL, 'Dhakari', '10912', '75');
+INSERT INTO `city` (`id`, `name`, `codes`, `state_id`) VALUES (NULL, 'Turmakhad', '10913', '75');
+INSERT INTO `city` (`id`, `name`, `codes`, `state_id`) VALUES (NULL, 'Sitad', '10914', '75');
+
+
+INSERT INTO `city` (`id`, `name`, `codes`, `state_id`) VALUES (NULL, 'Dhungad', '10915', '75');
+
+
+INSERT INTO `city` (`id`, `name`, `codes`, `state_id`) VALUES (NULL, 'Dandakot', '10916', '75');
